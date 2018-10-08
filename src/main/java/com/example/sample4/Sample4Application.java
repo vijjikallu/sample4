ace994891d97d56ba4fa492b81233cdb45ec81f5
unknown <bvijethareddy@gmail.com>
2018-10-08 11:52:46 -0400

    frist commit

---
+++ src/main/java/com/example/sample4/Sample4Application.java
@@ -0,0 +1,12 @@
+package com.example.sample4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample4Application.class, args);
+	}
+}
